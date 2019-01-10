e1a919df2c9c0bdeb9d00bbb2c3c1063b4345b0d
Nenetti <akinori1090@gmail.com>
2019-01-10 11:57:46 +0900

    GameObject継承

--- Player.py
+++ Player.py
@@ -1,45 +1,16 @@
 from Keyboard import *
 import pygame
+from GameObject import *
 
 
-class Enemy:
-    def __init__(self, x: float, y: float, width: float, height: float, image: pygame.Surface):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = image
-        self.speed_x = 0
-        self.speed_y = 0
-        self.animation = False
-        self.split_x = 0
-        self.split_y = 0
-        self.size_x = 0
-        self.size_y = 0
-        self.anim_no = 0
-        self.max_anim = 0
+class Player(GameObject):
+    def __init__(self, x: float, y: float, width: float, height: float, image: pygame.Surface = None):
+        super().__init__(x, y, width, height, image)
+        self.Controller = Keyboard()
 
-    def set_animation(self, split_x: int, split_y: int):
-        self.animation = True
-        self.split_x = split_x
-        self.split_y = split_y
-        self.size_x = self.width / split_x
-        self.size_y = self.height / split_y
-        self.max_anim = split_x * split_y
-
-    def move(self, speed_x: float, speed_y: float):
-        self.x += speed_x
-        self.y += speed_y
-
-    def update(self, screen):
+    def update(self, screen: pygame.Surface):
         self.calc_speed(screen)
-        if self.animation:
-            screen.blit(self.image, [self.x, self.y], [int(self.size_x*self.anim_no), 0, int(self.size_x), int(self.size_y)])
-            self.anim_no+=1
-            if self.anim_no>=self.max_anim:
-                self.anim_no=0
-        else:
-            screen.blit(self.image, [self.x, self.y])
+        super().update(screen)
 
     def calc_speed(self, screen: pygame.Surface):
         input = self.Controller.get_input()
