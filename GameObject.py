c373c7ece20815d8878cefae8e98ae2324f4da53
Nenetti <akinori1090@gmail.com>
2019-01-10 11:56:32 +0900

    抽象クラス作成

---
+++ GameObject.py
@@ -0,0 +1,33 @@
+import pygame
+from Animation import *
+
+
+class GameObject:
+    def __init__(self, x: float, y: float, width: float, height: float, image: pygame.Surface = None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.speed_x = 0
+        self.speed_y = 0
+        self.animation = None
+        self.is_animation = False
+
+    def set_animation(self, animation: Animation):
+        self.is_animation = True
+        self.animation = animation
+
+    def set_image(self, image: pygame.Surface):
+        self.image = image
+
+    def move(self, speed_x: float, speed_y: float):
+        self.x += speed_x
+        self.y += speed_y
+
+    def update(self, screen: pygame.Surface):
+        self.calc_speed(screen)
+        if self.is_animation:
+            self.animation.update(screen, self.x-self.width/2, self.y-self.height/2)
+        else:
+            screen.blit(self.image, [self.x-self.width/2, self.y-self.height/2])
